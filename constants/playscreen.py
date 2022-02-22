7e65ce3e666025b8260143280506d85e10445521
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-02-22 22:52:53 +0300

    Перенос обработки событий в отдельный файл

--- constants/playscreen.py
+++ constants/playscreen.py
@@ -1,7 +1,14 @@
 import pygame
+import objects.SpellCard
 
 from objects.glob import groups, events, screen
 from constants.window import *
+from objects.Enemy import Enemy
+from objects.Player import Player
+
+# Глобальные переменные
+player = None
+enemy = None
 
 
 class PlayField(pygame.sprite.Sprite):
@@ -18,9 +25,23 @@ class PlayField(pygame.sprite.Sprite):
 
 
 def init():
+    global player, enemy
     PlayField(FRAME_LEFT, FRAME_TOP, FRAME_RIGHT-FRAME_LEFT, FRAME_BOTTOM-FRAME_TOP)
     pygame.time.set_timer(events["enemy_attack_event"], 1000, 1)
+    player = Player()
+    enemy = Enemy(80)
 
 
 def handle(event):
+    global player, enemy
+    if player is None or enemy is None:
+        return
+    if event.type == pygame.USEREVENT:
+        player.reloaded = True
+        pygame.time.set_timer(events["reloaded_event"], 0)
+    if event.type == pygame.USEREVENT + 1:
+        enemy.attack()
+    if event.type == pygame.USEREVENT + 2:
+        objects.SpellCard.loop()
+
     screen.fill(BLACK)
--- main.py
+++ main.py
@@ -1,22 +1,17 @@
 import pygame
-import objects.SpellCard
 import constants.playscreen
 import constants.startscreen
 
 from constants.window import *
-from objects.Enemy import Enemy
-from objects.Player import Player
-from objects.glob import groups, events, screen
+from objects.glob import groups, screen
 
 # Инициализация
 pygame.init()
 pygame.mixer.init()
 pygame.display.set_caption("Haizen Project")
 clock = pygame.time.Clock()
-state = 0
-constants.startscreen.init()
-player = Player()
-enemy = Enemy(80)
+state = 1
+constants.playscreen.init()
 
 
 # Game loop
@@ -30,13 +25,6 @@ while running:
             constants.startscreen.handle(event)
         elif state == 1:
             constants.playscreen.handle(event)
-        if event.type == pygame.USEREVENT:
-            player.reloaded = True
-            pygame.time.set_timer(events["reloaded_event"], 0)
-        if event.type == pygame.USEREVENT + 1:
-            enemy.attack()
-        if event.type == pygame.USEREVENT + 2:
-            objects.SpellCard.loop()
 
     groups["all_sprites"].update()
     groups["all_sprites"].draw(screen)
