cbf1704e0c4d51ef5612b25642e75a855161aa06
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-02-23 19:47:36 +0300

    Изменение шрифта и добавление заготовки настроек

--- constants/startscreen.py
+++ constants/startscreen.py
@@ -13,9 +13,11 @@ DEF_BUTTON_X = WIDTH - 90
 
 
 def init():
-    Text("Haizen Project", "EoSD", 35, "white", (WIDTH / 2, HEIGHT / 4), "center")
-    buttons.append(Text("Start", "EoSD", 35, "grey", (DEF_BUTTON_X, HEIGHT / 2), "right"))
-    buttons.append(Text("Quit", "EoSD", 35, "grey", (DEF_BUTTON_X, HEIGHT / 2 + 40), "right"))
+    font = "Segoe Script"
+    Text("Haizen Project", font, 32, "white", (WIDTH / 2, HEIGHT / 4), "center")
+    buttons.append(Text("Start", font, 32, "grey", (DEF_BUTTON_X, HEIGHT / 2), "right"))
+    buttons.append(Text("Settings", font, 32, "grey", (DEF_BUTTON_X, HEIGHT / 2 + 40), "right"))
+    buttons.append(Text("Quit", font, 32, "grey", (DEF_BUTTON_X, HEIGHT / 2 + 80), "right"))
 
 
 def handle(event):
@@ -37,6 +39,8 @@ def handle(event):
                 constants.playscreen.init()
                 objects.glob.game_state = 1
                 return
+            elif selected == 1:
+                pass
             else: sys.exit()
 
     selected %= len(buttons)
