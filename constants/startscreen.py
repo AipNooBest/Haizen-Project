8710c398c1cefe7512eb301fb45737be258cba7c
AipNooBest <47401054+AipNooBest@users.noreply.github.com>
2022-02-23 18:37:14 +0300

    Перенос класса Text в отдельный файл

--- constants/startscreen.py
+++ constants/startscreen.py
@@ -1,45 +1,10 @@
 import sys
 import pygame
-
 import constants.playscreen
 import objects.glob
+
+from objects.Text import Text
 from constants.window import *
-
-
-class Text(pygame.sprite.Sprite):
-    def __init__(self, text, font, size, color, position, alignment):
-        pygame.sprite.Sprite.__init__(self)
-        self.text = text
-        self._font = font
-        self._size = size
-        self._color = color
-        self.surface = _text_format(text, font, size, color)
-        self.rect = self.surface.get_rect()
-        self.image = self.surface.subsurface(self.rect)
-        self.alignment = self._align(position, alignment)
-        self.add(objects.glob.groups["all_sprites"])
-
-    def change_text(self, text, font, size, color):
-        if text is None: text = self.text
-        if font is None: font = self._font
-        if size is None: size = self._size
-        if color is None: color = self._color
-        old_position = (self.rect.x, self.rect.y)
-        self.surface = _text_format(text, font, size, color)
-        self.rect = self.surface.get_rect()
-        self.image = self.surface.subsurface(self.rect)
-        self.alignment = self._align(old_position, self.alignment)
-
-    def _align(self, position, alignment):
-        if alignment == 'left':
-            self.rect.x, self.rect.y = position
-        elif alignment == 'center':
-            self.rect.centerx, self.rect.centery = position
-        elif alignment == 'right':
-            self.rect.topright = position
-        else:
-            raise AttributeError("Некорректное значение alignment")
-        return alignment
 
 
 selected = 0
@@ -51,12 +16,6 @@ def init():
     Text("Haizen Project", "EoSD", 35, "white", (WIDTH / 2, HEIGHT / 4), "center")
     buttons.append(Text("Start", "EoSD", 35, "grey", (DEF_BUTTON_X, HEIGHT / 2), "right"))
     buttons.append(Text("Quit", "EoSD", 35, "grey", (DEF_BUTTON_X, HEIGHT / 2 + 40), "right"))
-
-
-def _text_format(text, font, size, color):
-    new_font = pygame.font.SysFont(font, size)
-    new_text = new_font.render(text, False, color)
-    return new_text
 
 
 def handle(event):
---
+++ objects/Text.py
@@ -0,0 +1,44 @@
+import pygame
+import objects.glob
+
+
+class Text(pygame.sprite.Sprite):
+    def __init__(self, text, font, size, color, position, alignment):
+        pygame.sprite.Sprite.__init__(self)
+        self.text = text
+        self._font = font
+        self._size = size
+        self._color = color
+        self.surface = _text_format(text, font, size, color)
+        self.rect = self.surface.get_rect()
+        self.image = self.surface.subsurface(self.rect)
+        self.alignment = self._align(position, alignment)
+        self.add(objects.glob.groups["all_sprites"])
+
+    def change_text(self, text, font, size, color):
+        if text is None: text = self.text
+        if font is None: font = self._font
+        if size is None: size = self._size
+        if color is None: color = self._color
+        old_position = (self.rect.x, self.rect.y)
+        self.surface = _text_format(text, font, size, color)
+        self.rect = self.surface.get_rect()
+        self.image = self.surface.subsurface(self.rect)
+        self.alignment = self._align(old_position, self.alignment)
+
+    def _align(self, position, alignment):
+        if alignment == 'left':
+            self.rect.x, self.rect.y = position
+        elif alignment == 'center':
+            self.rect.centerx, self.rect.centery = position
+        elif alignment == 'right':
+            self.rect.topright = position
+        else:
+            raise AttributeError("Некорректное значение alignment")
+        return alignment
+
+
+def _text_format(text, font, size, color):
+    new_font = pygame.font.SysFont(font, size)
+    new_text = new_font.render(text, False, color)
+    return new_text
